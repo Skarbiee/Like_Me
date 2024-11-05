8dcff30a3040052f3077e68840c3963a165b307c
Skarbiee <garrigues.eva15@gmail.com>
2024-11-05 09:38:24 +0100

    Actualisation du javascript pour les boutons like

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-const switchjaime = document.querySelector('#icone');
+// Sélectionne tous les éléments avec l'id icone
+const switchjaime = document.querySelectorAll('#icone'); 
 
-// switchjaime.addEventListener('click', function () {
-//     document.querySelectorAll('span').classList.toggle('text-info');
-//     switchjaime.classList.toggle('fa-solid');
-// });
-
-function itemClicked(event) {
-    if (event.target.tagName === 'I') { // Vérifie que l'élément cliqué est un <i>
-        document.querySelector('span').classList.toggle('text-info');
-        switchjaime.classList.toggle('fa-solid');
-    }
-}
-switchjaime.addEventListener('click', itemClicked);
+// Ajoute un écouteur pour chaque icone
+// Sélectionne le <span> suivant l'icône
+// Ajoute ou retire la classe text-info
+// Bascule entre fa-regular et fa-solid
+switchjaime.forEach(icon => {                       
+    icon.addEventListener('click', function () {
+        const likeText = this.nextElementSibling;   
+        likeText.classList.toggle('text-info');     
+        this.classList.toggle('fa-solid');          
+    });
+});
