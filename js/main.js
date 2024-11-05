a9c94f5d378b2dd7fefa1b70a42af7fe3bd91e4f
Skarbiee <garrigues.eva15@gmail.com>
2024-11-05 13:39:56 +0100

    mise a jour du javascript

--- js/main.js
+++ js/main.js
@@ -1,14 +1,9 @@
-const switchjaime = document.querySelector('#icone');
+const switchjaime = document.querySelectorAll('#icone');
 
-// switchjaime.addEventListener('click', function () {
-//     document.querySelectorAll('span').classList.toggle('text-info');
-//     switchjaime.classList.toggle('fa-solid');
-// });
-
-function itemClicked(event) {
-    if (event.target.tagName === 'I') { // Vérifie que l'élément cliqué est un <i>
-        document.querySelector('span').classList.toggle('text-info');
-        switchjaime.classList.toggle('fa-solid');
-    }
-}
-switchjaime.addEventListener('click', itemClicked);
+switchjaime.forEach(icon => {
+    icon.addEventListener('click', function () {
+        const spanText = this.nextElementSibling;  // Sélectionne le <span> suivant l'icône
+        spanText.classList.toggle('text-info');
+        this.classList.toggle('fa-solid');
+    });
+});
